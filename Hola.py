8c20c4deace95dd44832ec6044b5378950b0d1f0
Melvin Ramos <128021731+melram11@users.noreply.github.com>
2024-09-12 04:34:23 +0000

    file que tenia que agregar

---
+++ Hola.py
@@ -0,0 +1,7 @@
+nombres = input ("Ingrese sus nombres: ")
+apellidos = input ("Ingrese sus apellidos: ")
+print ("Su nombre completo es: " + nombres + " " + apellidos)
+
+num1 = int ( input("Ingrese Numero 1:"))
+num2 = int ( input("Ingrese Numero 2:"))
+print ("La suma es: " + num1 + " " + num2)
